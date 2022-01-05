725987e2123c2b5119d091c79df2b09f86ab061a
ciscopilux <tranquocb306@gmail.com>
2022-01-05 16:20:47 +0700

    add event menu bar responsive mobile

---
+++ assets/js/js-menu-bar.js
@@ -0,0 +1,27 @@
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn')
+var header = document.getElementById('header')
+var headerHeight = header.clientHeight
+
+// mobileMenuBtn.addEventListener('click', function(){
+    
+// })
+
+mobileMenuBtn.onclick = function() {
+    var isClose = header.clientHeight === headerHeight
+    if (isClose) {
+        header.style.height = 'auto'
+    }
+    else {
+        header.style.height = null
+    }
+
+}
+
+// auto close after event click menu
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]')
+for(var i=0; i<menuItems.length; i++){
+    var menuItem = menuItems[i]
+    menuItem.onclick = function() {
+        header.style.height = null
+    }
+}
